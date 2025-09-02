d5a48d929209adb6df96cb03ac3ea776101c94bd
Abhay Rawat <rawatabhay54321@gmail.com>
2025-09-02 18:12:46 +0530

    local-time

--- script.js
+++ script.js
@@ -7,9 +7,13 @@ function Cityname(){
     else{   
          let response = fetch(`https://api.weatherapi.com/v1/current.json?key=6d0976c6d4114cd59cb123503252108&q=${city}&aqi=yes`).then((res)=>res.json()).then((data)=>{
             document.querySelector(".area").innerHTML= `${data.location.name}ˏ${data.location.region}ˏ${data.location.country}`
-            let date = data.current.last_updated;
-            date.trim(0,9)
-            document.querySelector(".date").innerHTML= `${date}`;
+            console.log(data)
+            let dateinfo = new Date(data.location.localtime);
+            let localtime = dateinfo.getHours().toString().padStart(2,"0");
+            let localmin = dateinfo.getMinutes().toString().padStart(2,"0");
+            // console.log(localtime,":",localmin);
+            console.log(dateinfo);
+            document.querySelector(".hour").innerHTML= `Time: ${localtime}:${localmin}`;
             document.querySelector(".condition").innerHTML= `${data.current.condition.text}`;
             document.querySelector(".temp").innerHTML= `${data.current.temp_c}°C`
             document.querySelector(".humidity").innerHTML = `${data.current.humidity}%`;
